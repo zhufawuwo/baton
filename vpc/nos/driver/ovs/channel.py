69dcf8ee2c9f88f0eeb7cd5a8b2020bfb6f37b90
l00254588 <david.liyi@huawei.com>
2018-01-20 09:27:13 +0800

    init

--- vpc/nos/driver/ovs/channel.py
+++ vpc/nos/driver/ovs/channel.py
@@ -2,21 +2,43 @@
 # coding:utf-8
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol,Factory,connectionDone
+
 from pub import *
+from openflow import OpenFlowProtocol as OFP
+
 
 class OpenFlowChannel(Protocol):
+    def __init__(self):
+        super().__init__()
+        self.versions = set(map(float,str(conf.get("protocol","versions")).split('|')))
+
     def dataReceived(self, data):
-        print(data)
+        ver,oftype,len,xid = OFP.parse_ofp_header(data)
+        if oftype == OFP.OFPT_HELLO :
+            self._handle_hello(data)
+
 
     def sendData(self,data):
         self.transport.write(data)
 
     def connectionMade(self):
-        print(self.transport)
-        print(self.transport.__dict__)
+        self._send_hello()
 
     def connectionLost(self, reason=connectionDone):
         pass
+
+    def _send_hello(self):
+        msg = OFP.hello(self.versions)
+        self.sendData(msg)
+
+    def _handle_hello(self,msg):
+        versions = OFP.parse_hello(msg)
+        accept_versions = self.versions
+        version = OFP.negotiate_version(versions,accept_versions)
+        if version :
+            pass
+        else :
+            self.sendData(OFP.hello_failed())
 
 
 
--- vpc/nos/driver/ovs/openflow.py
+++ vpc/nos/driver/ovs/openflow.py
@@ -1,16 +1,62 @@
 #! python3
 # coding: utf-8
 
+import struct
 from lib.openflow import base
 
 class OpenFlowProtocol(object):
+    OFPT_HELLO = 0
     def __init__(self):
         pass
 
     @classmethod
-    def parse_ofp_header(cls,message):
-        return base.parse_ofp_header(message)
+    def parse_ofp_header(cls,msg):
+        return base.parse_ofp_header(msg)
 
+    @classmethod
+    def hello(cls,versions,**kargs):
+        xid = kargs.get("xid",base.hms_xid())
+        if versions :
+            vset = base.ofp_version_normalize(versions)
+        else :
+            vset = set((1,))
+        version = max(vset)
+
+        if version < 4 :
+            return struct.pack("!BBHI", version, 0, 8, xid)
+        else :
+            units = [0, ] * (1 + version // 32)
+            for v in vset:
+                units[v // 32] |= 1 << (v % 32)
+
+            versionbitmap_length = 4 + len(units) * 4
+            fmt = "!BBHIHH%dI%dx" % (len(units), 8 * ((len(units) - 1) % 2))
+            return struct.pack(fmt, version, 0, struct.calcsize(fmt), xid,  # HELLO
+                              1, versionbitmap_length, *units)  # VERSIONBITMAP
+    @classmethod
+    def hello_failed(cls,accept_versions):
+        ascii_txt = "Accept versions: %s" % ["- 1.0 1.1 1.2 1.3 1.4".split()[x] for x in list(accept_versions)
+        return struct.pack("!BBHIHH", max(accept_versions), 1,
+                                  struct.calcsize("!BBHIHH") + len(ascii_txt), base.hms_xid(),
+                                  0, 0) + ascii_txt
+
+    @classmethod
+    def parse_hello(cls,msg):
+        return base.parse_hello(msg)
+
+    @classmethod
+    def negotiate_version(cls,versions,accept_versions):
+        client = base.ofp_version_normalize(versions)
+        server = base.ofp_version_normalize(accept_versions)
+        cross = client & server
+        if cross :
+            version = max(cross)
+            return version
+        return None
 
 if __name__ == "__main__":
-    pass
+    msg = b'\x04\x00\x00\x08\x00\x00\x00\x03'
+    ret = OpenFlowProtocol.parse_ofp_header(msg)
+    print(ret)
+    vers = [1.1,1.3]
+    print(OpenFlowProtocol.hello(vers))

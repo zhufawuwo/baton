4a75b6036b121a676b4e65adf9c26c34f0479ae9
l00254588 <david.liyi@huawei.com>
2018-01-25 23:49:49 +0800

    init

--- vpc/nos/driver/ovs/ne.py
+++ vpc/nos/driver/ovs/ne.py
@@ -3,7 +3,6 @@
 from vpc.nos import NetworkElement,NetworkElementEvent,event_t,EventChain
 
 class OVSEvent(NetworkElementEvent):
-
     def __init__(self,ne_id,type):
         super().__init__(ne_id,type)
 
